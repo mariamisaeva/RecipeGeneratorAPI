fb7852b043c403b965165e36c7e85d41711e2cff
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-11-14 18:53:47 +0100

    add

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,11 +13,11 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: ,
+  username: process.env.DB_USERNAME ,
     password: ,
     database: ,
     entities: ,
     synchronize: ,
     logging: ,
-    
+
 });
