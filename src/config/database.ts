c8cc1342fd5fcb68c9ea46ef3e9b248b1dd1a9a9
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-11-14 19:05:06 +0100

    add

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,11 +13,9 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME ,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [],
-    synchronize:true , 
-    logging: ,
-
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [],
+  synchronize: true, // this will auto create tables based on the entities
 });
