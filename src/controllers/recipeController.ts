396f2476150f1c9ba5e616de68c5487a9e4429f5
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-11-20 15:30:01 +0100

    Created getAllRecipes controller

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -1,24 +1,48 @@
 import { Request, Response } from 'express';
+import { AppDataSource } from '../config/database';
+import { Recipe } from '../entities/Recipe';
 
 //GetAllRecipes
-const getAllRecipes = async (req: Request, res: Response) => {};
+const getAllRecipes = async (req: Request, res: Response): Promise<void> => {
+  console.log('getAllRecipes Controller is Working...'); ////
 
+  try {
+    const recipeRepository = AppDataSource.getRepository(Recipe);
+    const fetchAllRecipes = await recipeRepository.find();
+
+    res.status(200).json({
+      success: true,
+      message: 'Getting all recipes...',
+      data: fetchAllRecipes,
+    });
+  } catch (err: any) {
+    // console.error(err); ////
+    // if (err instanceof Error) {
+    //   res.status(500).json({ success: false, message: err.message });
+    // }
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+//================================================================//
 //CreateRecipe
-const createRecipe = async (req: Request, res: Response) => {};
-
+const createRecipe = async (req: Request, res: Response): Promise<void> => {};
+//================================================================//
 //GetRecipeById
-const getRecipeById = async (req: Request, res: Response) => {};
-
+// const getRecipeById = async (req: Request, res: Response) => {};
+//================================================================//
 //UpdateRecipe //EditRecipe
-const updateRecipe = async (req: Request, res: Response) => {};
-
+// const updateRecipe = async (req: Request, res: Response) => {};
+//================================================================//
 //DeleteRecipe
-const deleteRecipe = async (req: Request, res: Response) => {};
+// const deleteRecipe = async (req: Request, res: Response) => {};
 
 export {
   getAllRecipes,
   createRecipe,
-  getRecipeById,
-  updateRecipe,
-  deleteRecipe,
+  //   getRecipeById,
+  //   updateRecipe,
+  //   deleteRecipe,
 };
---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export interface Recipe_TS {
+  title: string;
+  description?: string;
+  isVegetarian: boolean;
+  servings: number;
+  time: string;
+  image?: string;
+  category?: string;
+  recipeIngredients: string[];
+  recipeInstructions: string[];
+}
