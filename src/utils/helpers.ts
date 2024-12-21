e8865e082635187f480f4a28da9d0ed4d8e9e3a5
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-12-21 20:43:13 +0100

    remove unnecessary comments - helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -199,7 +199,7 @@ export const handleUpdateIngredients = async (
     const deletedIngredients = recipe.ingredients.filter(
       (ri) => !updatedIngredients.some((uRIng) => uRIng.id === ri.id),
     );
-    // console.log('Deleting RecipeIngredients:', deletedIngredients); ////
+
     await recipeIngredientRepository.remove(deletedIngredients);
   }
 
